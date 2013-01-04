5573098fc5066eac9c0f732910a3ef1eb7d42cf8
johan-hoglund <johan@jhoglund.se>
2013-01-05 00:37:39 +0100

    Replaced leaked FB App key, removed old user package

--- packages/addons/user/templates/sms_code_auth_form.tpl.php
+++
@@ -1,9 +0,0 @@
-<h1>Ange sms-kod</h1>
-<form method="post">
-	<input type="hidden" value="auth" name="action" />
-	<input type="hidden" name="phone_number" value="<?php echo $phone_number; ?>" />
-
-	<input type="text" name="sms_code" />
-	<input type="submit" value="Logga in" />
-</form>
-
